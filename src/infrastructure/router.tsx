708f6e84248f831a199c6113dbec664dd55019cc
Elises <yeghishe.kartashyan@gmail.com>
2024-09-26 21:17:56 +0200

    Add MasonryGrid component to router

--- src/infrastructure/router.tsx
+++ src/infrastructure/router.tsx
@@ -1,9 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
+import { loader as masonryGridLoader } from './masonry-grid.loader';
 import App from '../App';
+import MasonryGrid from '../ui/masonry-grid';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        path: '',
+        element: <MasonryGrid />,
+        loader: masonryGridLoader,
+      },
+    ],
   },
 ]);

7fd9cb47fc70ca3f3bb5e8022b9355b40ad76631
Elises <yeghishe.kartashyan@gmail.com>
2024-09-26 21:13:44 +0200

    Add UnsplashApiAdapter for implementing fetching details

---
+++ src/infrastructure/adapters/photo-repository.unsplash-api.adapter.ts
@@ -0,0 +1,40 @@
+import { Photo } from "../../domain/photo";
+import { PhotoRepository } from "../../domain/ports/photo-repository.port";
+import { BasicPhoto } from "./unsplash";
+
+const ACCESS_KEY = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
+
+export class UnsplashApiAdapter implements PhotoRepository {
+  async fetchPhotos(query: string) {
+    const response = await fetch(
+      `https://api.unsplash.com/search/photos?query=${query}&client_id=${ACCESS_KEY}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'max-age=604800', // Cache for 1 week
+      },
+      cache: 'force-cache',
+    }
+    );
+    const data = await response.json() as { results: BasicPhoto[] };
+    return data.results.map(PhotoMapper.toDomain);
+  }
+}
+
+class PhotoMapper {
+  static toDomain(photo: BasicPhoto): Photo {
+    return {
+      id: photo.id,
+      urls: {
+        small: photo.urls.small,
+        full: photo.urls.full,
+      },
+      user: {
+        name: photo.user.name,
+      },
+      description: photo.description || "",
+      alt_description: photo.alt_description || "",
+      created_at: photo.created_at,
+    };
+  }
+}

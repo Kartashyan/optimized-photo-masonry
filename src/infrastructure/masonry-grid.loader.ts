f43ba9a873d56186373c3204a377321af4b07ff6
Elises <yeghishe.kartashyan@gmail.com>
2024-09-30 23:51:09 +0200

    Refactor masonry-grid loader to handle query parameters in the loader function

--- src/infrastructure/masonry-grid.loader.ts
+++ src/infrastructure/masonry-grid.loader.ts
@@ -2,8 +2,10 @@ import { LoaderFunction } from "react-router-dom";
 import { PhotoService } from "../services/photo.service";
 import { UnsplashApiAdapter } from "./adapters/photo-repository.unsplash-api.adapter";
 
-export const loader: LoaderFunction = async () => {
-  const photoService = new PhotoService(new UnsplashApiAdapter()); // Inject the dependency here
-  const photos = await photoService.fetchPhotos("cats");
-  return { photos };
+export const loader: LoaderFunction = async ({ request }) => {
+  const photoService = new PhotoService(new UnsplashApiAdapter()); // Injecting the dependency here
+  const query = new URL(request.url).searchParams.get("query") || "cats";
+  const page = Number(new URL(request.url).searchParams.get("page")) || 1;
+  const result = await photoService.fetchPhotos(query, { page });
+  return result;
 };

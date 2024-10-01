ad9c9bf036fd31483c88c54b0bf8fd9a180a98c6
Elises <yeghishe.kartashyan@gmail.com>
2024-10-01 21:03:56 +0200

    Add photo fetching functionality for load more

--- src/ui/components/hooks/use-photos-query.ts
+++ src/ui/components/hooks/use-photos-query.ts
@@ -1,15 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Photo } from "../../../domain/photo";
 import { useInitialPhotosBeforeRender } from "./use-initial-photos-before-render";
+import { useFetcher } from "react-router-dom";
 
 export const usePhotosQuery = () => {
   const initialData = useInitialPhotosBeforeRender();
+  const fetcher = useFetcher();
 
-  const [photos] = useState<Photo[]>(initialData.photos);
+  const [photos, setPhotos] = useState<Photo[]>(initialData.photos);
+  const [pagination, setPagination] = useState<Record<string, number>>({ next: 2 });
+
+  useEffect(() => {
+    if (!fetcher.data || fetcher.state === "loading") {
+      return;
+    }
+    if (fetcher.data) {
+      const newPhotos = [...(fetcher.data.photos as Photo[])];
+      setPhotos((prev) => [...prev, ...newPhotos]);
+      setPagination(fetcher.data.pagination);
+    }
+  }, [fetcher, fetcher.data, fetcher.state]);
+
   const loadMore = () => {
     console.log("load more");
+    const page = pagination.next;
+    const hasMore = !!pagination.next;
+    if (!hasMore) {
+      console.log("no more");
+      return;
+    }
+    fetcher.load(`/photos?page=${page}`);
   };
+  const loading = fetcher.state === "loading";
 
-
-  return { photos, loading: false, loadMore };
+  return { photos, loading, loadMore };
 }

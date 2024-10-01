cf83430bd6f71ef79d2043e787527593d32c4939
Elises <yeghishe.kartashyan@gmail.com>
2024-10-01 14:34:17 +0200

    Refactor photo repository to include pagination options

--- src/domain/ports/photo-repository.port.ts
+++ src/domain/ports/photo-repository.port.ts
@@ -2,6 +2,7 @@ import { Photo } from "../photo";
 
 type Pagination = Record<string, string>;
 type FetchListResult<T> = { photos: T[]; pagination: Pagination };
+export type PhotoListResult = FetchListResult<Photo>;
 
 export interface PhotoRepository {
   fetchPhotos(query: string, options?: { page: number }): Promise<FetchListResult<Photo>>;

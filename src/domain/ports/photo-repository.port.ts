2e27e56a6e8401ce3426590c855d09162b84492a
Elises <yeghishe.kartashyan@gmail.com>
2024-10-03 11:37:57 +0200

    Refactor photo repository port interface to improve photo search and suport abort signal

--- src/domain/ports/photo-repository.port.ts
+++ src/domain/ports/photo-repository.port.ts
@@ -1,10 +1,17 @@
 import { Photo } from "../photo";
+// export type Pagination = {first: number; last: number; next?: number; prev?: number; };
 
-type Pagination = Record<string, number>;
+export type Pagination = Record<string, number>;
 type FetchListResult<T> = { photos: T[]; pagination: Pagination };
 export type PhotoListResult = FetchListResult<Photo>;
 
+export type PhotoSearch = {
+  page: number;
+  query: string;
+  per_page?: number;
+};
+
 export interface PhotoRepository {
-  fetchPhotos(query: string, options?: { page: number }): Promise<FetchListResult<Photo>>;
+  fetchPhotos(search: PhotoSearch, options?: { signal: AbortSignal }): Promise<FetchListResult<Photo>>;
   fetchPhotoById(id: string): Promise<Photo>;
 }

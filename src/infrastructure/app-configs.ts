9c333b34288f96447436233482bfe165e8991133
Elises <yeghishe.kartashyan@gmail.com>
2024-10-03 22:53:10 +0200

    refactor: expose Unsplash API URL in app-configs.ts

--- src/infrastructure/app-configs.ts
+++ src/infrastructure/app-configs.ts
@@ -1,5 +1,5 @@
 const ACCESS_KEY = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
-const API_URL = import.meta.env.VITE_UNSPLASH_API_URL;
+const API_URL = "https://api.unsplash.com/search/photos";
 
 export const appConfigs = {
   ACCESS_KEY,
@@ -15,8 +15,4 @@ export type AppConfigs = typeof appConfigs;
 
 if (!ACCESS_KEY) {
   throw new Error('Unsplash access key is missing');
-}
-
-if (!API_URL) {
-  throw new Error('Unsplash API URL is missing');
 }

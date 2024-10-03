7a7d2dcfad30bfd089979aa6b929c407a829412b
Elises <yeghishe.kartashyan@gmail.com>
2024-10-03 11:41:47 +0200

    Update photo service to support updated photo repository interface

--- src/services/photo.service.ts
+++ src/services/photo.service.ts
@@ -1,15 +1,22 @@
 import { Photo } from "../domain/photo";
 import { PhotoRepository, PhotoListResult } from "../domain/ports/photo-repository.port";
+import { UnsplashApiAdapter } from "../infrastructure/adapters/photo-repository.unsplash-api.adapter";
 
 export class PhotoService {
   constructor(private photoRepository: PhotoRepository) {
     this.photoRepository = photoRepository;
   }
-  async fetchPhotos(query: string, options: { page: number }): Promise<PhotoListResult> {
-    return this.photoRepository.fetchPhotos(query, options);
+  async fetchPhotos(search: {
+    page: number;
+    query: string;
+    per_page?: number;
+  }, options?: { signal: AbortSignal }): Promise<PhotoListResult> {
+    return this.photoRepository.fetchPhotos(search, options);
   }
 
   async fetchPhotoById(id: string): Promise<Photo> {
     return this.photoRepository.fetchPhotoById(id);
   }
 }
+
+export const photoService = new PhotoService(new UnsplashApiAdapter());

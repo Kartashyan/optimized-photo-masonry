80bd2aa8ef24700f6d7dc4a9acb00f7ab715439c
Elises <yeghishe.kartashyan@gmail.com>
2024-10-03 19:59:38 +0200

    feat: add rootMargin to detect intersection earlier to improve user experience

--- src/ui/components/masonry-grid.tsx
+++ src/ui/components/masonry-grid.tsx
@@ -89,10 +89,13 @@ export const MasonryGrid: React.FC<MasonryGridProps> = ({
       if (loading) return;
       if (observer.current) observer.current.disconnect();
       observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting && !loading) {
+        if (entries[0].isIntersecting) {
           loadMore();
         }
-      });
+      }, {
+        rootMargin: "400px",
+      }
+    );
       if (node) observer.current.observe(node);
     },
     [loading, loadMore]

95527a1662116a49c787e1b19b9b1e47fcd8681b
Elises <yeghishe.kartashyan@gmail.com>
2024-10-03 20:26:50 +0200

    Updates

--- src/ui/components/hooks/use-initial-photos-before-render.tsx
+++
@@ -1,7 +0,0 @@
-import { useLoaderData } from "react-router-dom";
-import { PhotoListResult } from "../../../domain/ports/photo-repository.port";
-
-export const useInitialPhotosBeforeRender = (): PhotoListResult => {
-  const data = useLoaderData() as PhotoListResult;
-  return data;
-};

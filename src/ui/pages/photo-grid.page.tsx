47d707e9ff7e8b250cb44be7f02fd3efde7973e8
Elises <yeghishe.kartashyan@gmail.com>
2024-10-03 11:51:40 +0200

    Refactor PhotoGridPage to include search functionality

--- src/ui/pages/photo-grid.page.tsx
+++ src/ui/pages/photo-grid.page.tsx
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { usePhotosQuery } from "../components/hooks/use-photos-query";
 import { MasonryGrid } from "../components/masonry-grid";
 
 export const PhotoGridPage: React.FC = () => {
-  const { photos, loadMore, loading } = usePhotosQuery();
+  const [searchText, setSearchTesxt] = useState("");
+  const { photos, loadMore, loading, error } = usePhotosQuery(searchText);
 
   const navigate = useNavigate();
 
   const handlePhotoClick = (id: string) => {
     navigate(`/photos/${id}`);
   };
-
   return (
-    <MasonryGrid
-      photos={photos}
-      onItemClick={handlePhotoClick}
-      loadMore={loadMore}
-      loading={loading}
-    />
+    <>
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => setSearchTesxt(e.target.value)}
+      />
+      <MasonryGrid
+        photos={photos}
+        onItemClick={handlePhotoClick}
+        loadMore={loadMore}
+        loading={loading}
+      />
+      {error && <p>{error.message}</p>}
+    </>
   );
 };

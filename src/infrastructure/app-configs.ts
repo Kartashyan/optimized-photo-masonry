006820de681616d9d7775ac11818766564bd7032
Elises <yeghishe.kartashyan@gmail.com>
2024-10-04 11:36:13 +0200

    feat: include imageGrid configs into appConfigs

--- src/infrastructure/app-configs.ts
+++ src/infrastructure/app-configs.ts
@@ -1,3 +1,4 @@
+
 const ACCESS_KEY = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
 const API_URL = "https://api.unsplash.com/search/photos";
 
@@ -5,13 +6,23 @@ export const appConfigs = {
   ACCESS_KEY,
   API_URL,
   search: {
-    perPage: 10,
+    perPage: 30,
     orderBy: 'latest',
     debounceDelay: 300,
+  },
+  imageGrid: {
+    columnWidth: 236,
+    gap: 8,
+    lazyLoadOffset: 1000,
+    loadOffset: 1200,
+    img: {
+      loading: "lazy",
+    } as Pick<HTMLImageElement, "loading">,
   },
 };
 
 export type AppConfigs = typeof appConfigs;
+export type ImageGridConfigs = AppConfigs['imageGrid'];
 
 if (!ACCESS_KEY) {
   throw new Error('Unsplash access key is missing');

cc53b01cd226b102c0801e9bb368e2c18353a1ec
Elises <yeghishe.kartashyan@gmail.com>
2024-10-04 17:06:41 +0200

    chore: Add rollup-plugin-visualizer to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer({
+    filename: './dist/stats.html',
+    open: false,
+  }),],
   test: {
     globals: true,
     environment: 'jsdom',

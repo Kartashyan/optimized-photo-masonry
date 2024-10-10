d2cb773c2ea0a5a7c81a34ec8fd5a3df615d4cf1
Elises <yeghishe.kartashyan@gmail.com>
2024-10-10 23:50:13 +0200

    refactor: Wrap App component with AppConfigsProvider
    Wrap the App component with the AppConfigsProvider to provide necessary configurations to child components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { Outlet } from 'react-router-dom';
-import styled from 'styled-components';
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+import { AppConfigsProvider } from "./ui/components/contexts/configs-context";
 
 const AppContainer = styled.div`
   margin: 0 auto;
 `;
 
 const App = () => (
-  <AppContainer>
-    <Outlet />
-  </AppContainer>
+  <AppConfigsProvider>
+    <AppContainer>
+      <Outlet />
+    </AppContainer>
+  </AppConfigsProvider>
 );
 
 export default App;
